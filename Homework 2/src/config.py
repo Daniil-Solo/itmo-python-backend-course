058c0e2a1a670f3dee0e9b5eeb9712e46110cea3
Daniil-Solo <daniil.solo1723@gmail.com>
2023-10-05 02:25:58 +0500

    add MODE with opportunity to test

--- Homework 2/src/config.py
+++ Homework 2/src/config.py
@@ -3,4 +3,10 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-DB_FILEPATH = os.environ["DB_FILEPATH"]
+TEST_MODE = os.environ["MODE"] == "TEST"
+DB_FILEPATH = os.environ['DB_FILEPATH']
+if TEST_MODE:
+    # тестируем в оперативной памяти
+    DB_URL = "sqlite+aiosqlite:///:memory:"
+else:
+    DB_URL = f"sqlite+aiosqlite:///{os.environ['DB_FILEPATH']}"
