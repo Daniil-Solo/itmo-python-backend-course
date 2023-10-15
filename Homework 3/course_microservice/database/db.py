7734b6bada78ae86f284fc5867193883655d7e14
Daniil-Solo <daniil.solo1723@gmail.com>
2023-10-16 01:48:41 +0500

    add models, create data for course-microservice

---
+++ Homework 3/course_microservice/database/db.py
@@ -0,0 +1,7 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from course_microservice.settings import settings
+
+
+db_url = f"sqlite+aiosqlite:///{settings.db_filepath}"
+engine = create_async_engine(db_url)
+DBSession = async_sessionmaker(engine)
