cbc2445704263379e1944f42e3c79495e37a5554
Daniil-Solo <daniil.solo1723@gmail.com>
2023-10-18 02:09:08 +0500

    add time_checking microservice

---
+++ Homework 3/time_checking/app.py
@@ -0,0 +1,18 @@
+import asyncio
+from grpc import aio
+from protos.time_checking_pb2_grpc import add_CheckTimeServiceServicer_to_server
+from time_checking.service.service import CheckTimeServicer
+from time_checking.settings import settings
+
+
+async def run_server():
+    server = aio.server()
+    add_CheckTimeServiceServicer_to_server(CheckTimeServicer(), server)
+    server.add_insecure_port(settings.time_checking_grpc_server_address)
+    await server.start()
+    print("Server has started")
+    await server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    asyncio.run(run_server())
